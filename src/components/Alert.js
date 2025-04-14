6c9c959c4a15649aefd0358ddb42216d9b413eba
oscar <oscar.javier741@gmail.com>
2025-04-13 21:50:16 -0500

    se agrega componente Alert.js

---
+++ src/components/Alert.js
@@ -0,0 +1,17 @@
+// src/components/Alert.js
+import React from 'react';
+import './Alert.css'; // Importa estilos si los tienes
+
+function Alert({ message }) {
+  if (!message) {
+    return null; // No mostrar nada si no hay mensaje
+  }
+
+  return (
+    <div className="alert">
+      {message}
+    </div>
+  );
+}
+
+export default Alert;
---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+// src/components/Button.js
+import React from 'react';
+
+function Button({ children, onClick }) {
+  return (
+    <button onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
